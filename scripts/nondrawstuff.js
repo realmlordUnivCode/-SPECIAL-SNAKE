21278b3094e89479bdfe109e84b57ccec37e8e55
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-12 14:28:15 -0500

    Create nondrawstuff.js

---
+++ scripts/nondrawstuff.js
@@ -0,0 +1,39 @@
+function move(){
+    
+while (alive == true){
+if(direction==up)
+    snakeY = snakeY + 10
+    snakeY2 = snakeY - length
+    
+    
+} 
+    if(direction==down){
+        
+        snakeY = snakeY + 10
+        
+        snakeY2 = snakeY - length
+        
+    
+}
+if(direction==right) {
+ snakeX= snakeX -10
+    
+    snakeX2 = snakeX - length
+    
+    
+}
+
+ if(direction==left){
+  
+     snakeX = snakeX + 10 
+     snakeX2 = snakeX + length
+     
+     
+     
+     
+ }
+    
+}
+function input(){
+ 
+    
