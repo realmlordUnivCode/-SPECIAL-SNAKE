f0a776611d803da5d85b38df0acfc31451254184
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-12 09:57:44 -0500

    Update food.js

--- scripts/food.js
+++ scripts/food.js
@@ -1,6 +1,6 @@
 
-var foodx = rand(constrain(width - 20)
-var foody = rand(constrain(height - 20)
+var foodX = rand(constrain(width - 20)
+var foodY = rand(constrain(height - 20)
 
 
 ctx.rect(foodX, foodY, 40, 40);
