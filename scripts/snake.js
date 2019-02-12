80841906d51384d643429eca47f84b4be9cbbe67
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-12 09:47:26 -0500

    Update snake.js

--- scripts/snake.js
+++ scripts/snake.js
@@ -1,10 +1,21 @@
+
+
 var snakeX = 20
 var snakeY = 20
 var nextSnakeX = 30
 var nextSnakeY = 30
 
 
+while (alive = true) { 
+  wait(20)
+  
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.rect(snakeX, snakeY, 40, 40);
 ctx.stroke();
+ 
+  snakeX =nextSnakeX
+  
+  snakeY = nextSnakeY
+  
+  
