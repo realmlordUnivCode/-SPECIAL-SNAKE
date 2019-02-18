d54287fb1273e37c4f162efe714447cbb5308ed3
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-17 20:21:14 -0500

    Create localStorageVars.js

---
+++ localStorageVars.js
@@ -0,0 +1,7 @@
+localStorage.snakeX = 0;
+localStorage.snakeY = 0;
+localStorage.snakeX2 = 20;
+localStorage.snakeY2 = 20;
+localStorage.length = 20;
+    localStorage.direction = "right":
+localStorage.alive = true;
