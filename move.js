ecdd64ed7edcf32c51f0f9aab65a579b88b01433
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-17 20:15:39 -0500

    Create move.js

---
+++ move.js
@@ -0,0 +1,29 @@
+/* Move this inside the brackets of move() */
+  if (localstorage.direction == "up") {
+
+    snakeY = snakeY + 10
+    snakeY2 = length
+      snakeX2 = 20
+  
+  }
+  if (localstorage.direction == "down") {
+
+    snakeY = snakeY - 10
+    snakeY2 = length
+  snakeX2 = 20
+  }
+  
+  if (localstorage.direction == "right") {
+
+      snakeX = snakeX + 10
+    snakeX2 = length
+  snakeY2 = 20
+  
+  }
+  if (localstorage.direction == "left") {
+
+    snakeX = snakeX - 10
+    snakeX2 = length
+  snakeY2 = 20
+ 
+  }
