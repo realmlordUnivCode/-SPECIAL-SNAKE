03add2d7c9c915e5150171f48a82e25cdce7e2fe
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-19 13:21:39 -0500

    Create vars.js

---
+++ vars.js
@@ -0,0 +1,22 @@
+var snakeX = 0;
+        var snakeY = 0;
+        var snakeX2 = 20;
+        var snakeY2 = 20;
+        var direction = "right";
+        var alive = true;
+        var length = 20
+      var foodCollected = true
+      
+        var foodX
+        var foodY
+       var snakeSeg2X = 0
+        var snakeSeg2Y = 0
+        var seg2XMovement= 0
+        var seg2YMovement= 0
+        var eg2Length = 0
+         var snakeXMove =0
+         var snakeYMove=0
+         var seg2Shrink=0
+         var seg2ShrinkVert=0
+
+   
