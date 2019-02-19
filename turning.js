8c289681465d75f03fa15da5209ffcc525736bd8
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-19 11:26:38 -0500

    Update turning.js

--- turning.js
+++ turning.js
@@ -1,3 +1,19 @@
+
+ function snake(){
+          window.snakeX += window.snakeYMove
+          window.snakeY += window.snakeXMove
+        window.snakeYHeight += window.yMovement
+        window.snakeXLength += window.xMovement
+       window.seg2Length =+ window.seg2Shrink
+       window.seg2Height =+window.seg2VertShrink  
+               background(0)
+                stroke(255)
+                fill(255)
+                
+                rect(window.snakeX,window.snakeY,window.snakeLength,window.snakeHeight
+                rect(window.seg2X,window.seg2Y,window.seg2Length,window.seg2Height)
+                
+        }
 function turn(){
    if(window.turnDirection == "right"){
    window.seg2Length = window.length - 20
