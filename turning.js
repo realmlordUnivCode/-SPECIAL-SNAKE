b63d52f443137c6e3c0a3db138337627f8a9d7d1
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-19 11:07:04 -0500

    Update turning.js

--- turning.js
+++ turning.js
@@ -1,8 +1,58 @@
 function turn(){
-
-
-
-
-
-
-}
+   if(direction == "right"){
+   seg2Length = length - 20
+           for(length/10-2){
+            seg2shrink = -10
+            seg2shrinkvert = 0
+                   seg2X = snakeX
+                   seg2Y = snakeY
+            snakeLength += 10 
+                   snake()
+                   
+    }}
+           if(direction =="left"){
+            seg2Length = length +20
+                   for(length/10-2){
+                           
+                    seg2Shrink = 10
+                           
+                           seg2ShrinkVert = 0
+                           seg2X = snakeX
+                           seg2Y = snakeY
+                           snakeLength +=10
+                           snake()
+                   }}
+                          
+                   if(direction =="up"){
+                    seg2Height = length+20       
+                   for(length/10-2){
+                           
+                           seg2ShrinkVert = 10
+                           seg2Shrink =0
+                           
+                           seg2X = snakeX
+                           seg2Y = snakeY
+                           snakeHeight -=10
+                           snake()
+                   }
+                        
+                   }
+                    
+          
+                    if(direction =="down"){
+                      seg2Height = length-20
+                            for(length/10=2)
+                                    seg2ShrinkVert = -10
+                            seg2Shrink=0
+                            seg2X = snakeX
+                            seg2Y = snakeY
+                            snakeHeight -=10
+                            snake()
+                            
+                    }}
+                   
+           }
+   }
+    
+   }
+  }
