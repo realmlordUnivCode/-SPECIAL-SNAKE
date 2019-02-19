af0af5a31f7feeab51a07b8094bd138a00fc5390
realmlordUnivCode <42391574+realmlordUnivCode@users.noreply.github.com>
2019-02-19 11:17:57 -0500

    Create GlobalVars.js

---
+++ GlobalVars.js
@@ -0,0 +1,20 @@
+var window.snakeX = 0;
+        var window.snakeY = 0;
+        var window.snakeX2 = 20;
+        var window.snakeY2 = 20;
+        var window.direction = "right";
+        var window.alive = true;
+        var window.length = 20
+      var window.foodCollected = true
+      
+        var window.foodX
+        var window.foodY
+       var window.snakeSeg2X = 0
+        var window.snakeSeg2Y = 0
+        var window.seg2XMovement= 0
+        var window.seg2YMovement= 0
+        var window.seg2Length = 0
+         var window.snakeXMove
+         var window.snakeYMove
+         var window.seg2Shrink
+         var window.seg2ShrinkVert
